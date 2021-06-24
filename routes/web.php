9267bb1251df2319d1229b1d088e5fede0d48e3c
Julia Leuenberger <leuenberger2101@gmail.com>
2021-06-24 23:30:14 +0200

    Class post created

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,22 +15,18 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view ('posts');
+
+
+    
+    return view ('posts', [
+        'posts' => $posts
+    ]);
 });
 
 Route::get('posts/{post}', function ($slug){
 
     return view('post', [
-        ''
+        'post' => Post::find($slug)
     ]);
-
-//    if(! file_exists($path = __DIR__ . "/../resources/posts/{$slug}.html")){
-//        return redirect('/');
-//    }
-//
-//    $post = cache()->remember("posts.{$slug}", 1200, fn() => file_get_contents($path));
-//
-//
-//    return view('post', ['post' => $post]);
 
 })->where('post', '[A-z_\-]+');
