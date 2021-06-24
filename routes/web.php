3b4b3bb4badc3c180633100dc083351175d98cf7
Julia Leuenberger <leuenberger2101@gmail.com>
2021-06-24 19:34:55 +0200

    Last commit

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/hello', function () {
-    return view ('welcome');
+Route::get('/', function () {
+    return view ('posts');
 });
+
+Route::get('posts/{post}', function ($slug){
+
+    return view('post', [
+        ''
+    ]);
+
+//    if(! file_exists($path = __DIR__ . "/../resources/posts/{$slug}.html")){
+//        return redirect('/');
+//    }
+//
+//    $post = cache()->remember("posts.{$slug}", 1200, fn() => file_get_contents($path));
+//
+//
+//    return view('post', ['post' => $post]);
+
+})->where('post', '[A-z_\-]+');
