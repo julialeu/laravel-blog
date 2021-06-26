f7781867c4639cf743dec589ad690523d956a264
Julia Leuenberger <leuenberger2101@gmail.com>
2021-06-27 00:06:50 +0200

    Ultimo commit

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -7,7 +7,12 @@
 
     <?php foreach ($posts as $post) : ?>
     <article>
-        <?= $post; ?>
+        <h1>
+            <?= $post->title; ?>
+        </h1>
+        <div>
+            <?= $post->excerpt; ?>
+        </div>
     </article>
     <?php endforeach; ?>
 </body>
