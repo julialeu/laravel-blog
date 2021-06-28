386e34dab191cdf16da865722820f259a6fe2e75
Julia Leuenberger <leuenberger2101@gmail.com>
2021-06-28 10:23:31 +0200

    Before migration

--- routes/web.php
+++ routes/web.php
@@ -25,8 +25,15 @@ Route::get('/', function () {
 
 Route::get('posts/{post}', function ($slug){
 
+
     return view('post', [
-        'post' => Post::find($slug)
+        'post' => Post::findOrFail($slug)
+
     ]);
 
-})->where('post', '[A-z_\-]+');
+});
+
+
+
+
+
